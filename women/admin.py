151a402f9a4273add5e766a92a35b7abbc939b45
Иса <obeme4849@gmail.com>
2026-04-16 12:10:39 +0500

    added search-function, filter-function, filter-class married/single in admin-panel.

--- women/admin.py
+++ women/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin, messages
 from .models import Women, Category
 
+
+class MarriedFilter(admin.SimpleListFilter):
+    title = 'Статус женщины'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('married', 'Замужем'),
+            ('single', 'Не замужем')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'married':
+            return queryset.filter(husband__isnull=False)
+        elif self.value() == 'single':
+            return queryset.filter(husband__isnull=True)
+
+
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'is_published', 'cat', 'brief_info')
@@ -9,6 +27,8 @@ class WomenAdmin(admin.ModelAdmin):
     list_editable = ('is_published', )
     list_per_page = 5
     actions = ['set_published', 'set_draft']
+    search_fields = ('title__starswith', 'cat__name')
+    list_filter = (MarriedFilter, 'cat__name', 'is_published', 'id')
 
     @admin.display(description='Краткое описание', ordering='content')
     def brief_info(self, women: Women):
